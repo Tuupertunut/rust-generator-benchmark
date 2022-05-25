d8fde31db90fb7357c8824a46c38ca5fddc33647
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2022-05-25 09:03:44 -0500

    Update create-report.sh
    created a copy of the violin plot and changed the black values to white for better integration with dark themes

--- create-report.sh
+++ create-report.sh
@@ -7,3 +7,5 @@ cp -r target/criterion report
 
 shopt -s globstar
 perl -i -pe 's|>generator-benchmark/\d\d |>|' report/**/*.svg report/**/*.html
+cp report/generator-benchmark/report/violin.svg report/generator-benchmark/report/violin-dark.svg
+sed -i 's/black/white/g' report/generator-benchmark/report/violin-dark.svg
